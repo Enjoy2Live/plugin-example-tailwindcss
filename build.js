56f30c822c7c1db3f86c0d09fa013af249328f9e
Enjoy2Live <iMohammedMaqbol@gmail.com>
2023-04-21 23:47:11 +0300

    viable!

--- build.js
+++ build.js
@@ -1,68 +1,113 @@
-const StyleDictionaryPackage = require('style-dictionary');
-const {createArray} = require('./fns');
+const StyleDictionaryPackage = require("style-dictionary");
+const { createArray } = require("./fns");
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
 StyleDictionaryPackage.registerFormat({
-  name: 'css/variables',
+  name: "css/variables",
   formatter: function (dictionary, config) {
-    return `${this.selector} {\n${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}\n}`
-  }
+    console.log(JSON.stringify(dictionary, null, 2));
+    return `${this.selector} {\n${dictionary.allProperties
+      .filter((prop) => prop.type !== "typography")
+      .map((prop) => `  --${prop.name}: ${prop.value};`)
+      .join("\n")}\n}`;
+  },
+});
+
+StyleDictionaryPackage.registerFormat({
+  name: "css/typography",
+  formatter: function (dictionary, config) {
+    return `${this.selector} {\n${dictionary.allProperties
+      .filter((prop) => prop.type === "typography")
+      .map((prop) => `  --${prop.name}: ${prop.value};`)
+      .join("\n")}\n}`;
+  },
 });
 
 StyleDictionaryPackage.registerTransform({
-    name: 'sizes/px',
-    type: 'value',
-    matcher: function(prop) {
-        // You can be more specific here if you only want 'em' units for font sizes    
-        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
-    },
-    transformer: function(prop) {
+  name: "sizes/px",
+  type: "value",
+  matcher: function (prop) {
+    // You can be more specific here if you only want 'em' units for font sizes
+    return [
+      "fontSizes",
+      "spacing",
+      "borderRadius",
+      "borderWidth",
+      "sizing",
+      "shadows",
+      "borders",
+      // "body",
+      // "heading",
+    ].includes(prop.attributes.category);
+  },
+  transformer: function (prop) {
+    const { original } = prop;
+    switch (prop.type) {
+      case "boxShadow":
+        return !Array.isArray(original.value)
+          ? `${original.value.type === "innerShadow" ? "inset" : ""} ${
+              original.value.x
+            } ${original.value.y} ${original.value.blur} ${
+              original.value.spread
+            } ${original.value.color}`
+          : original.value.map(
+              (val) =>
+                `${val.type === "innerShadow" ? "inset" : ""} ${val.x} ${
+                  val.y
+                } ${val.blur} ${val.spread} ${val.color}`
+            );
+        break;
+      default:
         // You can also modify the value here if you want to convert pixels to ems
-        return parseFloat(prop.original.value) + 'px';
+        return parseFloat(prop.original.value) + "px";
+        break;
     }
-    });
+  },
+});
 
 function getStyleDictionaryConfig(theme) {
   return {
-    "source": [
-      `tokens/${theme}.json`,
-    ],
-    "format": {
-      createArray
+    source: [`tokens/${theme}.json`],
+    format: {
+      createArray,
     },
-    "platforms": {
-      "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `output/`,
-        "files": [{
-          "destination": `${theme}.json`,
-          "format": "createArray"
-        }, {
-          "destination": `${theme}.css`,
-          "format": "css/variables",
-          "selector": `.${theme}-theme`
-        }]
-      }
-    }
+    platforms: {
+      web: {
+        transforms: ["attribute/cti", "name/cti/kebab", "sizes/px"],
+        buildPath: `output/`,
+        files: [
+          {
+            destination: `${theme}.json`,
+            format: "createArray",
+          },
+          {
+            destination: `${theme}.css`,
+            format: "css/variables",
+            selector: `.${theme}-theme`,
+          },
+        ],
+      },
+    },
   };
 }
 
-console.log('Build started...');
+console.log("Build started...");
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-['global', 'dark', 'light'].map(function (theme) {
+["senf"].map(function (theme) {
+  console.log("\n==============================================");
+  console.log(`\nProcessing: [${theme}]`);
 
-    console.log('\n==============================================');
-    console.log(`\nProcessing: [${theme}]`);
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    getStyleDictionaryConfig(theme)
+  );
 
-    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
+  StyleDictionary.buildPlatform("web");
 
-    StyleDictionary.buildPlatform('web');
+  console.log("\nEnd processing");
+});
 
-    console.log('\nEnd processing');
-})
-
-console.log('\n==============================================');
-console.log('\nBuild completed!');
+console.log("\n==============================================");
+console.log("\nBuild completed!");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,52 @@
-
 const { filterTokensByType } = require("./fns");
-const tokens = require("./output/light.json")
 
-const colors = filterTokensByType('color', tokens)
+const tokens = require("./output/senf.json");
 
+const extractTokenIndex = (tokens) =>
+  Object.fromEntries(
+    Object.entries(tokens).map(([key, value]) => {
+      const keyArr = key.split('-');
+      const removedKey = keyArr.shift();
+      return [keyArr.join(), value]
+    })
+  );
+
+const colors = filterTokensByType("color", tokens);
+const typography = filterTokensByType("typography", tokens);
+const fontFamily = extractTokenIndex(filterTokensByType("fontFamilies", tokens));
+const fontWeight = extractTokenIndex(filterTokensByType("fontWeights", tokens));
+const lineHeight = extractTokenIndex(filterTokensByType("lineHeights", tokens));
+const fontSize = extractTokenIndex(filterTokensByType("fontSizes", tokens));
+const letterSpacing = extractTokenIndex(filterTokensByType("letterSpacing", tokens));
+const borderWidth = extractTokenIndex(filterTokensByType("borderWidth", tokens));
+const opacity = extractTokenIndex(filterTokensByType("opacity", tokens));
+const boxShadow = {
+  ...filterTokensByType("boxShadow", tokens).shadows,
+  ...filterTokensByType("boxShadow", tokens).borders,
+};
+const borderRadius = extractTokenIndex(filterTokensByType("borderRadius", tokens));
+const textIndent = filterTokensByType("dimension", tokens); // how would we use this?
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
+  content: ['./index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors,
+    fontFamily,
+    fontWeight,
+    lineHeight,
+    fontSize,
+    letterSpacing,
+    borderWidth,
+    opacity,
+    boxShadow,
+    borderRadius,
+    textIndent,
+    extend: {
+      typography: ({ theme }) => console.log(theme.theme),
+    },
   },
   variants: {},
   plugins: [],
-}
+};
